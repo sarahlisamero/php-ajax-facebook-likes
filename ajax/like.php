4f43fd47189355092e88af842357c020c3288afa
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-04-20 12:30:28 +0200

    feat: add ajax likes to post

--- ajax/like.php
+++ ajax/like.php
@@ -1,19 +1,27 @@
 <?php
     require_once("../bootstrap.php");
     if( !empty($_POST) ) {
-        $postId = $_POST['postId'];
-        $userId = 1;
+        $postId = $_POST['id'];
+        $userId = 1; //dit zou gelijk moeten zijn aan de session wnr het niet hard coded is
 
         $l = new Like();
         $l->setPostId($postId);
         $l->setUserId($userId);
         $l->save();
 
+        $p = new Post();
+        $p-> id=$postId;
+        $likes = $p->getLikes();
+        /*var_dump($likes);
+        die();
+        1) requests debuggen in network tab in devtools, bij response*/
+
         $result = [
             "status" => "success",
-            "message" => "Like was saved"
+            "message" => "Like was saved",
+            "likes" => $likes
         ];
 
-        echo json_encode($result);
+        echo json_encode($result); //zodat je eraan kan in frontend -> omzetten naar js stukje
 
     }
--- index.php
+++ index.php
@@ -24,10 +24,45 @@
 	<article>
 		<p><?php echo $post->text; ?></p>
 		<img src="https://picsum.photos/300/200?random=<?php echo rand(1, 10000); ?>" alt="">
-		<div><a href="#" data-id="<?php echo $post->id; ?>" class="like">Like</a> <span class='likes'><?php echo $post->getLikes(); ?></span> people like this </div>
+		<div><a href="#" data-id="<?php echo $post->id; ?>" class="like">Like</a> <span class='likes' id="likes<?php echo $post->id;?>"><?php echo $post->getLikes(); ?></span> people like this </div>
 	</article>
 	<?php endforeach; ?>
+		<script>
+			//add event click on all links with class .like
+			let links = document.querySelectorAll(".like");
+			for (let i = 0; i < links.length; i++) {
+				links[i].addEventListener("click", function(e){
+					e.preventDefault();
+					//console.log("like clicked");
+					//get the id of the post
+					let id = this.getAttribute("data-id");
 
+					//get the span with the id likes{id}
+					let likes = document.querySelector("#likes"+id);
+					//console.log(id);
+
+					//fetch (POST) to ajax/like.php, use formdata
+					let formData = new FormData();
+					formData.append("id", id);
+					fetch("ajax/like.php", {
+						method: "POST",
+						body: formData
+					})
+					.then(function(response){
+						return response.json();
+					})
+					.then(function(json){
+						console.log(json); /*like requests checken in network tab in devtools: 
+						1) network leegmaken liken knop indrukken 
+						2) 200 & groen betekent aangekomen
+						3) dan klikken op de request & bij het response tabje kijken -> warning ofzo? er is iets mis*/
+						//update the likes
+						span.innerhtml = json.likes;
+					})
+
+				});
+			}
+		</script>
 
 
 
